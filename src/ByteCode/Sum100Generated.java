473c0a9b3dcc9b15a9923159d2bc7d53c8787aaf
Andreas Andersson <bambismurfen@gmail.com>
2016-02-23 15:50:49 +0100

    Bytecode- Exempel

---
+++ src/ByteCode/Sum100Generated.java
@@ -0,0 +1,19 @@
+package ByteCode;
+
+/**
+ * Created by Andreas on 2016-02-23.
+ */
+public class Sum100Generated {
+    public Sum100Generated() {
+    }
+
+    public static void main(String[] var0) {
+        int var1 = 0;
+
+        for(int var2 = 1; var2 <= 100; ++var2) {
+            var1 += var2;
+        }
+
+        System.out.println(Integer.toString(var1));
+    }
+}
